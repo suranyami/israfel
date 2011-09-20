37c7cbb765cedc16ab873786bcd133db7648ae1d
David Parry <david.parry@suranyami.com>
2011-09-20 10:15:32 +1000

    Commented out test that was causing infinite loop. Don't know why it's happening.

--- test/polygon-spec.coffee
+++ test/polygon-spec.coffee
@@ -9,13 +9,16 @@ describe "Polygon", ->
   describe "toPath", ->
     it "should be of the form M1 2 L2 2 L2 1Z", ->
       (expect @polygon.toPath()).toMatch /^M[0-9]+ [0-9]+ L[0-9]+ [0-9]+ L[0-9]+ [0-9]+z/
-  
-  describe "with a point inside", ->
-    it "has pointInside() true", ->
-      for x in [1.9..1.5] by 0.1
-        (expect @polygon.pointInside(new Point x, x)).toEqual true
-  
+
+  # 
+  # describe "with a point inside", ->
+  #   it "has pointInside() true", ->
+  #     for x in [1.9..1.5] by 0.1
+  #       (expect @polygon.pointInside(new Point x, x)).toEqual true
+  # 
   describe "with a point outside", ->
     it "has pointInside() false", ->
       for x in [1, 3, 10]
+        console.log x
+        console.log @polygon.pointInside(new Point x, x)
         (expect @polygon.pointInside(new Point x, x)).toEqual false
--- test/polygon-spec.js
+++ test/polygon-spec.js
@@ -8,16 +8,6 @@
         return (expect(this.polygon.toPath())).toMatch(/^M[0-9]+ [0-9]+ L[0-9]+ [0-9]+ L[0-9]+ [0-9]+z/);
       });
     });
-    describe("with a point inside", function() {
-      return it("has pointInside() true", function() {
-        var x, _results, _step;
-        _results = [];
-        for (x = 1.9, _step = 0.1; 1.9 <= 1.5 ? x <= 1.5 : x >= 1.5; x += _step) {
-          _results.push((expect(this.polygon.pointInside(new Point(x, x)))).toEqual(true));
-        }
-        return _results;
-      });
-    });
     return describe("with a point outside", function() {
       return it("has pointInside() false", function() {
         var x, _i, _len, _ref, _results;
@@ -25,6 +15,8 @@
         _results = [];
         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
           x = _ref[_i];
+          console.log(x);
+          console.log(this.polygon.pointInside(new Point(x, x)));
           _results.push((expect(this.polygon.pointInside(new Point(x, x)))).toEqual(false));
         }
         return _results;
