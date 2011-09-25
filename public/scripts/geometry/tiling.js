d288d74e96ebdde5f2f4c5aad12ee992f7079471
David Parry <david.parry@suranyami.com>
2011-09-25 14:58:35 +1000

    compiled.

---
+++ public/scripts/geometry/tiling.js
@@ -0,0 +1,8 @@
+(function() {
+  var Tiling;
+  window.Tiling = Tiling = (function() {
+    function Tiling() {}
+    Tiling.prototype.render = function(element) {};
+    return Tiling;
+  })();
+}).call(this);
